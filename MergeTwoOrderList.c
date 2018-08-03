91c0153d711ed7b669cb709f81d442a52504cc8d
何进鸿 <askunix@163.com>
2018-08-03 23:09:53 +0800

    合并两个有序链表-非递归

--- MergeTwoOrderList.c
+++ MergeTwoOrderList.c
@@ -1,2 +1,51 @@
 //合并两个有序链表
 
+
+
+ListNode* MergeTwoOrderList(ListNode* L1, ListNode* L2)
+{
+	
+	if(L1 == NULL && L2 == NULL)
+	{
+		return LL;
+	}
+	if(L1 == NULL)
+	{
+		retrun L2;
+	}
+	if(L2 == NULL)
+	{
+		return L1;
+	}
+	
+	ListNode* cur = new ListNode(0);
+	ListNode* newList = cur;
+	
+	while(L1 != NULL && L2 != NULL)
+	{
+		if(L1->val > L2->val)
+		{
+			cur->next = L2;
+			L2 = L2->next;
+		}
+		
+		else
+		{
+			cur->next = L1;
+			L1 = L1->next;
+		}
+		
+		cur = cur->next;
+	}
+	
+	if(L1 == NULL)
+	{
+		cur->next = L2;
+	}
+	else
+	{
+		cur->next = L1;
+	}
+	
+	return newList->next;
+}
