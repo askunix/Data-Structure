f8bdaf70e98e9c008a5b0d9b596b632059de3855
何进鸿 <askunix00@gmail.com>
2020-10-15 00:40:54 +0800

    std      vector string

--- 力扣算法/2020_10_14.cpp
+++ 力扣算法/2020_10_14.cpp
@@ -3,11 +3,14 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+	//*********************************    std::string     *******************************
+
 	std::string s1 = "hello helooo hello ,  hello";
 	size_t index3 = s1.find_last_of("hello");   //返回最后一个索引
 	cout << index3 << endl;
@@ -40,6 +43,55 @@ int main()
 		offindex1 = s1.find("hello", offindex1);
 	}
 	cout << "替换后的字符串：" << s1 << endl;
+
+	//*********************************    Vector     *******************************
+	//vector在尾部添加或者移除元素非常快，在中间操作非常耗时，因为它需要移动元素
+
+	//定义一个vector容器
+	vector<int> v1;
+
+	//插入元素(尾部插入)
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+
+	//迭代器遍历打印
+	for (vector<int>::iterator it = v1.begin(); it != v1.end(); it++) {
+		cout << *it << " ";
+	}
+	cout << endl;
+
+	//修改头部元素的值(front()返回是引用，可以当左值)
+	v1.front() = 44;
+
+	//输出头部元素
+	cout << "头部元素:" << v1.front() << endl;
+
+	//修改尾部的值(back()返回是引用，可以当左值)
+	v1.back() = 99;
+
+	//输出尾部元素
+	cout << "尾部元素" << v1.back() << endl;
+
+	//删除元素(从尾部删除)
+	v1.pop_back();
+
+	//迭代器遍历打印
+	for (vector<int>::iterator it = v1.begin(); it != v1.end(); it++) {
+		cout << *it << " ";
+	}
+	cout << endl;
+
+	//存放三个元素，每个元素都是9
+	vector<int> v5(3, 9);
+
+	//遍历-迭代器逆向遍历
+	for (vector<int>::reverse_iterator it = v1.rbegin(); it != v1.rend(); it++) {
+		cout << *it << " ";
+	}
+	cout << endl;
+
+
 
 
 
