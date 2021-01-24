018594d0d6e5fa7a8a7baa6682f3582236a8ad95
askunix <askunix00@gmail.com>
2021-01-24 13:35:17 +0800

    更新

---
+++ 力扣算法/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+
+class Operation
+{
+
+
+public:
+	double NumA
+	{
+		double get() { return _num;  }
+	};
+
+private:
+	double _numberA = 0;
+	double _numberB = 0;
+};
+
+
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
